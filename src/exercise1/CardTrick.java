621177e8f922d3cacd8a768dba00c67fba708eb8
kau13055 <kau13055@sheridancollege.ca>
2023-01-27 16:47:41 -0500

    ICE_1 Version1

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -86,6 +86,8 @@ public class CardTrick {
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
      * @author Paul Bonenfant Jan 2022
+     * 
+     * I am Done
      */
     private static void printInfo() {
     
